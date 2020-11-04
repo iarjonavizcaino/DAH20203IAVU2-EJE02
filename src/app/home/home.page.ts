fcd68593eabf0b04b9b530d8840795073a26a5d3
iarjonavizcaino <iarjona@ittepic.edu.mx>
2020-11-04 07:39:22 -0700

    Filter and delete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public places = [];
+  public search = '';
 
+
+  constructor() {
+    //consulta remota
+    this.places.push({
+      image: 'https://www.tepic.tecnm.mx/images/escudo_itt_200x200.png',
+      title: 'El Charco Verde',
+      subtitle: 'Santa María del Oro',
+      description: 'Este lugar tiene una cascada y puedes practicar deportes extremos',
+      active: true
+    });
+
+    this.places.push({
+      image: 'https://www.tepic.tecnm.mx/images/escudo_itt_200x200.png',
+      title: 'El Charco Verde 2',
+      subtitle: 'Santa María del Oro 2',
+      description: 'Este lugar tiene una cascada y puedes practicar deportes extremos 2',
+      active: true
+    });
+
+    this.places.push({
+      image: 'https://www.tepic.tecnm.mx/images/escudo_itt_200x200.png',
+      title: 'El Charco Verde 3',
+      subtitle: 'Santa María del Oro 3',
+      description: 'Este lugar tiene una cascada y puedes practicar deportes extremos 3',
+      active: true
+    });
+
+  }
+
+  filter(): void {
+    if (this.search && this.search.trim()) {
+      this.places = this.places.filter( (place) => {
+        return (place.title.toLocaleLowerCase().indexOf(this.search.toLocaleLowerCase()) > -1 );
+      });
+    }
+  }
+
+  deletePlace(position: number): void {
+    this.places.splice(position, 1);
+  }
+
+  changeStatus(position: number) {
+    this.places[position].active = !this.places[position].active;
+  }
 }
